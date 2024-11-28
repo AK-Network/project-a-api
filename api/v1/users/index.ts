29ad91f6a724141e6e8f4354c8e59f2eb1343769
AK superAdmin <ak.network.gr@gmail.com>
2024-11-28 16:43:01 +0200

    remove mongodb from edge function

--- api/v1/users/index.ts
+++ api/v1/users/index.ts
@@ -1,14 +1,13 @@
 import type { RequestContext } from '@vercel/edge';
-import { getAllUsers } from '../../../src/lib/db';
 
 export const config = {
   runtime: 'edge'
 }
 
 export default async function handler(req: Request, ctx: RequestContext) {
-  const users = await getAllUsers()
+  // const users = await getAllUsers()
 
-  return new Response(JSON.stringify({ name: 'Project A API', version: 1, resource: 'User', data: users }), {
+  return new Response(JSON.stringify({ name: 'Project A API', version: 1, resource: 'User' }), {
     headers: {
       'Content-Type': 'application/json'
     }
