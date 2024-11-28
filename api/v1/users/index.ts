63792334254c758479ae2be261f7bb7b045c19e7
AK superAdmin <ak.network.gr@gmail.com>
2024-11-28 15:19:44 +0200

    rechange file stracture

--- api/v1/users/index.ts
+++ api/v1/users/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getAllUsers } from '../../../lib/db';
+import { getAllUsers } from '../../../src/lib/db';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const users = await getAllUsers()
